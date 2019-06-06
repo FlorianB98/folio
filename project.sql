c967417e11a6a571c3c0981fd5f9e0c5113dd787
FlorianB98 <florianbrunet88@gmail.com>
2019-06-06 14:42:47 +0200

    add content

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 25 mai 2019 à 11:27
+-- Généré le :  jeu. 06 juin 2019 à 11:11
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `portfolio_elo`
+-- Base de données :  `portfolio_slim`
 --
 
 -- --------------------------------------------------------
@@ -34,18 +34,22 @@ CREATE TABLE IF NOT EXISTS `project` (
   `name1` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
   `introduction` text NOT NULL,
-  `description` varchar(600) NOT NULL,
+  `description` text NOT NULL,
   `category` varchar(50) NOT NULL,
   `lien` varchar(50) DEFAULT NULL,
+  `git` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `project`
 --
 
-INSERT INTO `project` (`id`, `name1`, `title`, `introduction`, `description`, `category`, `lien`) VALUES
-(1, 'dog', 'Dog', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean blandit erat sed dui gravida, ac elementum odio porttitor. Duis semper vel purus a dapibus. ', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean blandit erat sed dui gravida, ac elementum odio porttitor. Duis semper vel purus a dapibus. Praesent iaculis, elit a luctus ultricies, augue arcu varius justo, eget commodo ante eros ultrices enim. In bibendum eros condimentum nunc elementum rutrum. Nam eget dapibus ligula. Donec id est ut velit laoreet blandit vitae eget nisl. Donec tristique ornare egestas. Ut in posuere magna, nec iaculis tortor. Duis vitae est magna. In vestibulum neque quis consectetur pretium.', 'design', '');
+INSERT INTO `project` (`id`, `name1`, `title`, `introduction`, `description`, `category`, `lien`, `git`) VALUES
+(2, 'hermes', 'Hermès', 'Application réalisé en deux mois pour Hermès dans l\'entreprise RS2I.', 'Lors de mon stage de deux mois en 2017 dans l\'entreprise RS2I, j\'ai eu la chance de participer à la conception et à la réalisation d\'une application pour l\'entreprise de luxe française Hermès. Cette application a pour permets objectifs de surveiller chaque livraison des différents processus d\'Hermès.', 'dev', NULL, NULL),
+(3, 'star_maze', 'Star Maze', 'Un travail THREE JS sur le thème de l\'espace qui génère aléatoirement un labyrinthe en 3D et vous propose de le résoudre. ', 'Un projet scolaire pour HETIC ayant pour objectif de nous faire utilisé THREE JS (un environnement web 3D) sur le thème de l\'espace. J\'ai donc réalisé en deux semaines ce labyrinthe généré aléatoirement.', 'dev', NULL, 'https://github.com/FlorianB98/star_maze'),
+(4, 'war_voices', 'War Voices', 'Projet réalisé à 5 en 10 jours lors d\'une semaine intensive à HETIC sur le thème de l\'Histoire.', 'Ce projet a été réalisé à 5 en 10 jours lors d\'une semaine intensive à HETIC sur le thème de l\'Histoire.', 'dev', 'http://warvoices.aymericm.fr/', NULL),
+(5, 'ygo_trading', 'Yugioh Trading!', 'Projet réalisé à 5 en 4 jours dans le cadre d\'une semaine intensive à HETIC.', 'Projet réalisé à 5 en 4 jours dans le cadre d\'une semaine intensive à HETIC.', 'dev', 'https://www.ygo-trading.com/', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
